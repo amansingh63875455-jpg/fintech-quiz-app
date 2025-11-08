1732d05b6b23898ec9b9efee3c9093876bbfe4c6
amansingh63875455-jpg <amansingh63875455@gmail.com>
2025-11-08 12:06:34 +0530

    Delete unnecessary index.tsx with merge conflicts

--- index.tsx
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-export default function Home() {
-  return <h1>Welcome to My FinTech news App!</h1>;
-}
-=======
-export default function Home() {
-  return <h1>Welcome to My FinTech news App!</h1>;
-}
->>>>>>> 8375da77412cab9a553b0300b6082bc121f31f90
